32801d4283b567b4ad4e7da309437f4f0189419d
paulicstj <paulacastillojmz@gmail.com>
2024-10-20 22:49:00 -0500

    del2

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('/Users/pauli/Documents/python_projects/vehicles/vehicles_us.csv') 
+car_data = pd.read_csv('vehicles_us.csv') 
 
 st.header('Visualización de datos de vehículos en venta')
 
-hist_button= st.button('Construir histograma')
+hist_button= st.button('Construir histograma.')
 
 if hist_button:
-    st.write('Creación de un histograma para el conjunto de anuncios de venta de coches')
+    st.write('Creación de un histograma.')
 
     fig= px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width= True)
